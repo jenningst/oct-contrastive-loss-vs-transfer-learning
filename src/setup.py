e3b0df28b17b44cf6ae129afd684766667066786
Troy Jennings <troy.c.jennings@gmail.com>
2022-08-10 16:04:35 -0600

    Final setup module

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,20 @@
+import os
+
 import gdown
 
 # define path to downloads and the outputs
 SIMCLR_URL = "https://drive.google.com/drive/folders/1HkC1tWVU0Q3weIQup5GQjJYeiADS_VwV?usp=sharing"
 INCEPTION_URL = "https://drive.google.com/drive/folders/1B__QjvTdzk-wJoRPCXgHYcncwT5-kDjm?usp=sharing"
 
-INCEPTION_OUTPUT = "./model/saved/inceptionv3_net"
-SIMCLR_OUTPUT = "./model/saved/simclrv2_net"
+MODEL_PATH = "./model/saved"
+INCEPTION_OUTPUT = os.path.join(MODEL_PATH, "inceptionv3_net")
+SIMCLR_OUTPUT = os.path.join(MODEL_PATH, "simclrv2_net")
 
 if __name__ == "__main__":
+    print("Building model path ...")
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(MODEL_PATH)
+
     # download local files and update the models
     print("Downloading models ...")
     gdown.download_folder(SIMCLR_URL, output=SIMCLR_OUTPUT, quiet=False, use_cookies=False)
